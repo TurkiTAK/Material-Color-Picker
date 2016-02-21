1c2352341af1e85d2aa04694c6f252a6e25793dc
Turki Alkhateeb <turki.khateeb@gmail.com>
2016-02-21 22:54:22 +0300

    Added files via upload

---
+++ src/main/java/com/turkialkhateeb/materialcolorpicker/ColorListener.java
@@ -0,0 +1,10 @@
+package com.turkialkhateeb.materialcolorpicker;
+
+import android.view.View;
+
+/**
+ * Created by TurkiAlkhateeb on 1/17/16.
+ */
+public interface ColorListener{
+    public void OnColorClick(View v, int color);
+}
